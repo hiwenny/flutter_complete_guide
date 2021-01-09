05e340bfdde91996cd97807699117506bac5cef5
hiwenny <wenny.h@live.com>
2021-01-09 22:36:56 +1100

    Trying out Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,17 @@ void main() {
   runApp(BaseApp());
 }
 
+// Alternatively:
+// void main() => runApp(BaseApp());
+
 class Example {
   String name;
   int age;
 
-  Example(String inputName, int age, {String optionalVar1='default', int optionalVar2=0, @required String namedValue3}) {
+  Example(String inputName, int age,
+      {String optionalVar1 = 'default',
+      int optionalVar2 = 0,
+      @required String namedValue3}) {
     name = inputName;
     age = this.age;
     print(optionalVar1);
@@ -23,10 +29,23 @@ class Example {
 // Text is a Widget from Material
 class BaseApp extends StatelessWidget {
   // decorator from Dart. Explicitly states that we are overriding StatelessWidget's build method with our implementation.
+  // Good practice is to explicitly say @override, even though it works fine without.
+  // This clarifies that we didn't rewrite over existing method by accident.
   @override
   Widget build(BuildContext ctx) {
     Example('Test', 30, namedValue3: 'String 3');
 
-    return MaterialApp(home: Text('Hellow'));
+    // return MaterialApp(home: Text('Hellow'));
+
+    // Scaffold is another MaterialUI Widget to create page quickly.
+    // AppBar is also from MatUI
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('Default body text'),
+      ),
+    );
   }
 }
