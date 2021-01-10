d53b0c3e3acee205d7647b31201e46a2435415fb
hiwenny <wenny.h@live.com>
2021-01-10 20:51:07 +1100

    Conditionals and ternary

--- lib/main.dart
+++ lib/main.dart
@@ -21,53 +21,71 @@ class BaseApp extends StatefulWidget {
 // _ is how we privatise classes/properties/etc
 class _BaseAppState extends State<BaseApp> {
   var _questionIndex = 0;
+
+  // questions is actually storing pointer
+  final questions = const [
+    {
+      'questionText': 'Whatchu doing?',
+      'answers': ['Mmmm?', 'Bluey', 'Good boy']
+    },
+    {
+      'questionText': 'Pretty birdy?',
+      'answers': ['Bluey\'s a good boy']
+    },
+    {
+      'questionText': 'Want some seebs?',
+      'answers': ['Chirp', 'Monch', 'Flies away']
+    },
+  ];
+
   void _answerQuestion() {
-    // IMPORTANT
-    // Without this, the internal state will still change, but doesn't re-render.
-    // a.k.a doesn't run build() again.
-    // Use this for changes that require rebuilds.
-    setState(() {
-      _questionIndex += 1;
-    });
-    print('This is an answer! $_questionIndex');
+    // To check if exist: _questionIndex != null
+    // Ternary is same as JS. condition ? return 0 : return 1;
+    if (_questionIndex < questions.length) {
+      // IMPORTANT
+      // Without this, the internal state will still change, but doesn't re-render.
+      // a.k.a doesn't run build() again.
+      // Use this for changes that require rebuilds.
+      setState(() {
+        _questionIndex += 1;
+      });
+      print('This is an answer! $_questionIndex');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-
-    var questions = [
-      {
-        'questionText': 'Whatchu doing?',
-        'answers': ['Mmmm?', 'Bluey', 'Good boy']
-      },
-      {
-        'questionText': 'Pretty birdy?',
-        'answers': ['Bluey\'s a good boy']
-      },
-      {
-        'questionText': 'Want some seebs?',
-        'answers': ['Chirp', 'Monch', 'Flies away']
-      },
-    ];
+    // It's possible to do var questions = const ['something']
+    // What is it for? It prevents mutations, like:
+    // var dummy = const ['Hello']
+    // dummy.add('Max') // will error because .add() tries to mutate the original array
+    // Good practice to prefix const where immutable.
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Stateful one'),
         ),
-        body: Column(children: [
-          Question(questions[_questionIndex]['questionText']),
-          // This is because Dart doesn't support list-in-object :( so need to cast it into List<type>
-          // Spread operator is as in JS
-          ...(questions[_questionIndex]['answers']as List<String>).map((answer) {
-            return Answer(_answerQuestion, answer);
-          }).toList()
-          // RaisedButton(
-          //   child: Text(questions[_questionIndex][
-          //       'questionText']),
-          //   onPressed: _answerQuestion,
-          // ),
-        ]),
+        body: _questionIndex < questions.length
+            ? Column(children: [
+                Question(questions[_questionIndex]['questionText']),
+                // This is because Dart doesn't support list-in-object :( so need to cast it into List<type>
+                // Spread operator is as in JS
+                ...(questions[_questionIndex]['answers'] as List<String>)
+                    .map((answer) {
+                  return Answer(_answerQuestion, answer);
+                }).toList() // because map<T> returns an Iterable<T> instead of a List<T>
+
+                // Above is a modularised version of
+                // RaisedButton(
+                //   child: Text(questions[_questionIndex][
+                //       'questionText']),
+                //   onPressed: _answerQuestion,
+                // ),
+              ])
+            : Center(
+                child: Text('Seebs!'),
+              ),
       ),
     );
   }
--- lib/question.dart
+++ lib/question.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class Question extends StatelessWidget {
   // String questionText;
   final String
-      questionText; // final tells the compiler the passed-in value is mutable,
-  // but once rendered here it's immutable -- final -- so it's fine and according to Stateless uses.
+      questionText; // final tells the compiler that though the passed-in value is mutable,
+  // but in this instance, once rendered, it's immutable -- final -- so it's fine and according to Stateless uses.
   // https://news.dartlang.org/2012/06/const-static-final-oh-my.html
+
+  // TLDR final is RUNTIME constant, const is compile time and runtime constant.
 
   Question(this.questionText); // This is the constructor
 
